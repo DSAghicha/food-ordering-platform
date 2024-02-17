f4a889ddb258ede8171476cc25e084be918c6883
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-02-17 20:36:19 +0530

    Footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import { Logo } from ".";
+
+const Footer: FC = () => {
+    return (
+        <footer className="bg-orange-500 py-10">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <span className="text-3xl text-white font-bold tracking-tight">
+                    <Logo />
+                </span>
+                <span className="text-white font-bold tracking-tight flex gap-4">
+                    <span>Privacy Policy</span>
+                    <span>Terms of Service</span>
+                </span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Footer } from "./Footer.tsx";
 export { default as Header } from "./Header";
 export { default as Hero } from "./Hero.tsx";
 export { default as Logo } from "./Logo";
