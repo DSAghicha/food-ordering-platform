490ae085e790026eee26da38706898390bcf86dd
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-02-17 18:25:54 +0530

    Header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import { Logo, MobileNav } from ".";
+
+const Header: FC = () => {
+    return (
+        <header className="border-b-2 border-b-orange-500 py-6">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link
+                    className="text-3xl font-bold tracking-tight text-orange-500"
+                    to="/"
+                >
+                    <Logo />
+                </Link>
+                <div className="md:hidden">
+                    <MobileNav />
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
