313d9d811747355ce13879d08bd682b33917143a
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-02-17 20:30:01 +0530

    Export Alias for Hero component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from "./Header";
+export { default as Hero } from "./Hero.tsx";
 export { default as Logo } from "./Logo";
 export { default as MainNav } from "./MainNav.tsx";
 export { default as MobileNav } from "./MobileNav.tsx";
