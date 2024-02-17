0852d27a8b5ce81fe6c640b076caa6ef4d9ab18f
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-02-17 22:20:09 +0530

    Configuring Auth0 component wrapper

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import { Auth0ProviderWithNavigate } from "./auth";
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <AppRoutes />
+            <Auth0ProviderWithNavigate>
+                <AppRoutes />
+            </Auth0ProviderWithNavigate>
         </BrowserRouter>
     </React.StrictMode>
 );
