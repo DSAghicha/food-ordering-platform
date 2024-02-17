078cd7955ce4ef979e0fcd5826023ce75e4a9b54
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-02-17 22:20:21 +0530

    Integrating auth0 login

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,11 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { FC } from "react";
 import { Button } from "./ui/button";
 
 const MainNav: FC = () => {
+    const { loginWithRedirect } = useAuth0();
     return (
         <Button
             variant="ghost"
             className="font-bold text-xl hover:text-orange-500 hover:bg-white"
+            onClick={async () => await loginWithRedirect()}
         >
             Login
         </Button>
