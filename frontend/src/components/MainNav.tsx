d79fcaa42c5dcdb77500f92a90c62c58099d4b8c
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-02-18 07:36:35 +0530

    Updated to show authenticated links

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,17 +1,24 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { FC } from "react";
+import { UserMenu } from ".";
 import { Button } from "./ui/button";
 
 const MainNav: FC = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
     return (
-        <Button
-            variant="ghost"
-            className="font-bold text-xl hover:text-orange-500 hover:bg-white"
-            onClick={async () => await loginWithRedirect()}
-        >
-            Login
-        </Button>
+        <span className="flex space-x-2 items-center">
+            {isAuthenticated ? (
+                <UserMenu />
+            ) : (
+                <Button
+                    variant="ghost"
+                    className="font-bold text-xl hover:text-orange-500 hover:bg-white"
+                    onClick={async () => await loginWithRedirect()}
+                >
+                    Login
+                </Button>
+            )}
+        </span>
     );
 };
 
