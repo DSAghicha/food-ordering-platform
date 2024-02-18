c5cadd1895d6dfc5b13ffc5cc21f0eea50a44b9b
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-02-18 07:37:05 +0530

    Update to show authenticated links

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,5 +1,7 @@
-import { Menu } from "lucide-react";
+import { LogoutOptions, useAuth0 } from "@auth0/auth0-react";
+import { CircleUserRound, Menu } from "lucide-react";
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import { Logo } from ".";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
@@ -12,6 +14,7 @@ import {
 } from "./ui/sheet";
 
 const MobileNav: FC = () => {
+    const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
     return (
         <Sheet>
             <SheetTrigger>
@@ -19,17 +22,67 @@ const MobileNav: FC = () => {
             </SheetTrigger>
             <SheetContent className="space-y-3">
                 <SheetTitle>
-                    Welcome to <Logo />
+                    {isAuthenticated && user ? (
+                        <span className="flex items-center font-bold gap-2">
+                            {user.picture ? (
+                                <img
+                                    src={user.picture}
+                                    className="rounded-full w-[40px] h-[40px]"
+                                    alt="pp"
+                                />
+                            ) : (
+                                <CircleUserRound className="text-orange-500" />
+                            )}
+                            {user.email}
+                        </span>
+                    ) : (
+                        <>
+                            Welcome to <Logo />
+                        </>
+                    )}
                 </SheetTitle>
                 <Separator />
-                <SheetDescription className="flex">
-                    <Button className="flex-1 font-bold bg-orange-500">
-                        Login
-                    </Button>
+                <SheetDescription className="flex flex-col gap-4">
+                    {isAuthenticated ? (
+                        <MobileNavLinks logout={logout} />
+                    ) : (
+                        <Button
+                            className="flex-1 font-bold bg-orange-500"
+                            onClick={async () => await loginWithRedirect()}
+                        >
+                            Login
+                        </Button>
+                    )}
                 </SheetDescription>
             </SheetContent>
         </Sheet>
     );
+};
+
+const MobileNavLinks: FC<MobileNavLinksProp> = ({
+    logout,
+}: MobileNavLinksProp) => {
+    return (
+        <>
+            <Link
+                to="/user-profile"
+                className="font font-bold hover:text-orange-500"
+            >
+                User Profile
+            </Link>
+            <Separator />
+            <Button
+                className="flex flex-1 font-bold bg-orange-500"
+                onClick={() => logout()}
+            >
+                Logout
+            </Button>
+        </>
+    );
+};
+
+type MobileNavLinksProp = {
+    logout: (options?: LogoutOptions | undefined) => Promise<void>;
 };
 
 export default MobileNav;
