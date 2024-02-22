54f72e3f0bd4ce178e13d81b2f0894f482d3ad8b
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-02-22 20:46:41 +0530

    Implemented route redirection to myUser router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,10 @@ import "dotenv/config";
 import express, { Request, Response, json, raw, urlencoded } from "express";
 import mongoose from "mongoose";
 import { version } from "../package.json";
+import { myUserRoutes } from "./routes";
 
 mongoose
-    .connect(process.env.MONGODB_URL as string)
+    .connect(process.env.MONGODB_URL as string, { dbName: "mern_eats" })
     .then(() => console.log("Connected to MongoDb"));
 
 const app = express();
@@ -18,5 +19,7 @@ app.use(cors());
 app.get("/", (_: Request, res: Response) => {
     res.send(`Welcome to our restaurant! ${version}`);
 });
+
+app.use("/api/my/user", myUserRoutes);
 
 app.listen(7000, () => console.log(`Server is up at 7000`));
