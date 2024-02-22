cda36a52308b3f607539801c46ca88c89ebc5974
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-02-22 22:54:01 +0530

    Added request logging to file

---
+++ backend/src/utils/fileLogger.ts
@@ -0,0 +1,20 @@
+import dayjs from "dayjs";
+import { appendFileSync, existsSync, mkdirSync } from "fs";
+import { resolve } from "path";
+
+const dateTimeStamp = dayjs().format("DD/MM/YY HH:mm:ss");
+
+const reqLog = (message: string): void => {
+    const logDirPath = resolve(__dirname, "../../logs");
+    if (!existsSync(logDirPath)) {
+        mkdirSync(logDirPath, { recursive: true });
+    }
+    const logFilePath = `${logDirPath}/request_${dayjs().format(
+        "DDMMYYYY"
+    )}.log`;
+    console.log(logFilePath);
+    const log = `[${dateTimeStamp}] ${message}\n`;
+    appendFileSync(logFilePath, log);
+};
+
+export default { reqLog };
